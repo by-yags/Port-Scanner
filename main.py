c306bbe1feb53b39c1ed1b06ef1b4219f670f385
ByYags <jehpentester@gmail.com>
2025-07-18 20:17:06 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from socket import *
+
+def conScan(tgtHost, tgtPort):
+    try:
+        connskt = socket(AF_INET, SOCK_STREAM)
+        connskt.connect((tgtHost, tgtPort))
+        print('[*]%d/tcp open'% tgtPort)
+        connskt.close()
+    except:
+        print('[*]%d/tcp closed'% tgtPort)
+        
+def portScan(tgtHost, tgtPorts):
+    try:
+        tgtIP = gethostbyname(tgtHost)
+    except:
+        print('[-] Cannot resolve %s ' % tgtHost)
+        return
+    try:
+        tgtName = gethostbyaddr(tgtIP)
+        print('\n[+] Scan Result of: %s' % tgtName[0])
+    except:
+        print('\n[+] Scan Result of: %s' % tgtIP)
+        
+    setdefaulttimeout(1)
+    for tgtPort in tgtPorts:
+        print('Scanning port %d' % tgtPort)
+        conScan(tgtHost, int(tgtPort))
+               
+if __name__ == '__main__':
+    portScan('google.com', [21, 22, 23, 25, 80, 110, 143, 443, 8080])
